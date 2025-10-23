0c0cf8eee4d7cb605462fbc4fd56af5d21540500
Ulukai85 <ueffeldie7@gmx.de>
2025-10-23 19:09:05 +0200

    Add NavBar component with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Grid, GridItem } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}>
-      <GridItem area={'nav'} bg={'coral'}>
-        Nav
+      <GridItem area={'nav'}>
+        <NavBar />
       </GridItem>
       <GridItem hideBelow='lg' area={'aside'} bg={'gold'}>
         Aside
