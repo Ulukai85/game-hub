f769041c8289d3f3db920cd240bdca4b01eb68fb
Ulukai85 <ueffeldie7@gmx.de>
2025-11-02 14:56:08 +0100

    Refactor entities

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import APIClient from '@/services/api-client';
 import useGameQueryStore from '@/store';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import ms from 'ms';
-import type { Game } from '../entities/Game';
+import type Game from '../entities/Game';
 
 const apiClient = new APIClient<Game>('/games');
 
